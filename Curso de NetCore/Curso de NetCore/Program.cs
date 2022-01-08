15963994477276bebc96a1443b46282f1dd67770
Andres29091 <Andres29091@gmail.com>
2022-01-08 12:39:37 -0500

    Curso de NET Core - Se continua curso de Net Core.

--- Curso de NetCore/Curso de NetCore/Program.cs
+++ Curso de NetCore/Curso de NetCore/Program.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace MyApp // Note: actual namespace depends on the project name.
 {
   public class Program
   {
     public static void Main(string[] args)
     {
+      //TIPO DE VARIABLES//
+
       //int year = Convert.ToInt32("12");
       //double decimales = 2.5;
       //string nombre = "Felipe";
@@ -22,11 +20,111 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
       //-----------------------------------------------------------------//
 
-      int valor1 = 3;
-      int valor2 = 6;
-      var suma = valor1 + valor2;
+      //OPERADORES ARITMETICOS//
 
-      Console.WriteLine( "La suma entre {0} y {1} es: {2} ", valor1, valor2, suma);
+      //int valor1 = 3;
+      //int valor2 = 6;
+      //var suma = valor1 + valor2;
+
+      //Console.WriteLine( "La suma entre {0} y {1} es: {2} ", valor1, valor2, suma);
+
+      //-----------------------------------------------------------------//
+
+      //OPERADORES DE ASIGNACION (+, -, *, /, %,) //
+
+      //int valor1 = 3;
+      //int valor2 = 6;
+      //valor2 += valor1;
+      //valor2 -= valor1;
+      //valor2 *= valor1;
+      //valor2 /= valor1;
+      //valor2 %= valor1;
+
+      //Console.WriteLine("Resultado: {0}", valor2);
+
+      //-----------------------------------------------------------------//
+
+      //OPERADORES DE COMPROBACION DE TIPOS Y RELACIONALES ( >=, >, <=, < )//
+
+      //int valor1 = 5;
+      //int valor2 = 10;
+      //var data = valor1 <= valor2;
+
+      //Console.WriteLine("Resultado: {0}", data);
+
+      //-----------------------------------------------------------------//
+
+      //OPERADORES DE IGUALDAD ( ==, != )//
+
+      //int valor1 = 5;
+      //int valor2 = 10;
+      //var data = valor1 != valor2;
+
+      //Console.WriteLine("Resultado: {0}", data);
+
+      //-----------------------------------------------------------------//
+
+      //ESTRUCTURA IF Y LOS OPERADORES CONDICIONALES//
+
+      //int value = 5;
+      //int value2 = 10;
+      //double value3 = 6;
+      //double value4 = 3;
+      //var data = value != value2;
+
+      //if (value != value2 || value3 == value4)
+      //{
+      //  Console.WriteLine("The condition is true");
+      //}
+      //else
+      //{
+      //  Console.WriteLine("The condition is false");
+      //}
+
+      //-----------------------------------------------------------------//
+
+      //ESTRUCTURA IF ANIDADA//
+
+      //int value = 5;
+      //int value2 = 10;
+      //double value3 = 12;
+      //double value4 = 152;
+      //var data = value == value2;
+
+      //if (value != value2 || value3 == value4)
+      //{
+      //  Console.WriteLine("The condition #1 is true");
+
+      //  if (data)
+      //  {
+      //    Console.WriteLine("The condition #2 is true");
+      //  }
+      //  else
+      //  {
+      //    Console.WriteLine("The condition #2 is false");
+      //  }
+      //}
+      //else
+      //{
+      //  Console.WriteLine("The condition #1 is false");
+      //}
+
+      //-----------------------------------------------------------------//
+
+      //OPERADOR CONDICIONAL//
+
+      //int value = 5;
+      //int value2 = 10;
+      //double value3 = 12;
+      //double value4 = 152;
+      //string name = String.Empty;
+      //var data = value == value2;
+
+      //name = data ? "Andrés" : "Felipe";
+
+      //Console.WriteLine("Resultado {0} ", name);
+
+      //-----------------------------------------------------------------//
 
       Console.ReadLine();
     }
